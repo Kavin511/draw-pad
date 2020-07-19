691b5b9272c4195935ca588b40d297b07ef7227c
Kavin S <51694233+Kavin511@users.noreply.github.com>
2020-07-19 20:37:04 +0530

    added touch events

--- index.js
+++ index.js
@@ -71,9 +71,43 @@ canvas.addEventListener("mousedown",()=>{ k=true;
    draw()})
 canvas.addEventListener("mouseup",()=>{ k=false;
    ctx.beginPath();})
+
+
 canvas.addEventListener("mousemove",draw)
    canvas.addEventListener("mouseout",()=>{k=false,ctx.beginPath()})
 
+   canvas.addEventListener("touchstart",()=>{ k=true;
+      draw()})
+   canvas.addEventListener("touchend",()=>{ k=false;
+      ctx.beginPath();})
+      canvas.addEventListener("touchmove",(e)=>{mousePos=getTouchPos(canvas,e)
+      let touch=e.touches[0]
+   let mouseEvent=new mouseEvent("mouseDown",{
+      clientX:touch.clientX,
+      clientY:touch.clientY
+
+   });
+canvas.dispatchEvent(mouseEvent)})
+canvas.addEventListener("touchend", function (e) {
+   var mouseEvent = new MouseEvent("mouseup", {});
+   canvas.dispatchEvent(mouseEvent);
+ }, false);
+ canvas.addEventListener("touchmove", function (e) {
+   var touch = e.touches[0];
+   var mouseEvent = new MouseEvent("mousemove", {
+     clientX: touch.clientX,
+     clientY: touch.clientY
+   });
+   canvas.dispatchEvent(mouseEvent);
+ }, false);
+ function getTouchPos(canvasDom, touchEvent) {
+   var rect = canvasDom.getBoundingClientRect();
+   return {
+     x: touchEvent.touches[0].clientX - rect.left,
+     y: touchEvent.touches[0].clientY - rect.top
+   };
+ }
+   // canvas.addEventListener("mouseout",()=>{k=false,ctx.beginPath()})
 
 }
 
